bbeb3f8c79169a7ec1c3e48ff24298bca7d76817
Milia-K <milyamilya20@gmail.com>
2023-10-15 17:48:14 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 8],
+  ["Eat", 2],
+  ["Study",3],
+  ["Relax", 2],
+  ["Sleep", 8],
+  ["Read", 1],
+];
+
+export const options = {
+  title: "My Day",
+};
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
 
